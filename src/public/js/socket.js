591728d6838d4de359a206e50db5a08455082a7e
yunjeong <yj2171063@ewhain.net>
2025-03-07 19:17:18 +0900

    배포용

--- src/public/js/socket.js
+++ src/public/js/socket.js
@@ -1,12 +1,8 @@
-// import { io } from "socket.io-client";
 
-// const socket = io({
-//     transports: ["websocket"], // WebSocket 연결 유지 (불필요한 HTTP polling 방지)
-//     reconnection: true,        // 연결 끊어지면 자동 재연결
-//     reconnectionAttempts: 5,   // 최대 5번 재연결 시도
-//     reconnectionDelay: 1000    // 1초 후 재연결 시도
-// });
 
-// export default socket;
-const socket = io("http://15.164.211.147:3000"); // 서버에서 자동으로 클라이언트 라이브러리를 제공
+const socket = io("ws://15.164.211.147:3000", { 
+    transports: ["websocket"], // 🚀 WebSocket 전용
+    upgrade: false, // 🚀 polling 차단 (WebSocket만 사용)
+});
+
 export default socket;
